5d25d554a4c6c5b766982e71dff97837e47ec9c4
Administrator <Administrator@DESKTOP-OBSFV9Q>
2018-12-05 12:08:31 +0800

    push project

---
+++ src/SubStr.java
@@ -0,0 +1,13 @@
+
+public class SubStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("112233".substring(0, 2).equals("11"));
+		
+		
+	}
+
+}
